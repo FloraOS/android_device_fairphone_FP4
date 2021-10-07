2537271a62d62ebba1520ab00ecc82745279034c
Jayasri Bhattacharyya <jbhattac@codeaurora.org>
2021-10-07 19:04:36 +0530

    rootdir: Fix duplicate sysprop assignment for rild.libpath.
    Additionally, append vendor. to the property to enable
    rild properly.
    
    Change-Id: Iffc64527de7b318b8dc51a83117470648c19073c
    (cherry picked from commit 41d0918420ab669fd75b37bd9d74e284e36fdc0c)

--- device.mk
+++ device.mk
@@ -833,7 +833,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.sib16_support=1 \
     persist.vendor.radio.custom_ecc=1 \
-    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
+    vendor.rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
     persist.vendor.radio.procedure_bytes=SKIP \
     persist.vendor.radio.rat_on=combine
 
