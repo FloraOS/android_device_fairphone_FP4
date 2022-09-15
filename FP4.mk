93987835b2004c8de8c3a54b6c04b5d4f48086d7
Bharath <bharath@fairphone.com>
2022-09-15 20:37:00 +0530

    FP4: Enforce all modules to exist
    * Use the AOSP macro to check if all modules that are added in
    PRODUCT_PACKAGES actually exist in our source or not.
    * Add product_manifest.xml Browser2 to allowed list since
    product_manifest.xml does not get built for all targets and
    Browser2 does not exist in the tree.
    
    Issue: FP3-A11#404
    Issue: FP4-INT#17
    Change-Id: Ic5572d68ed9f654033a1b1d65cd9054f22d723d4

--- FP4.mk
+++ FP4.mk
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (C) 2024 Fairphone B.V.
 
+allowed_list += product_manifest.xml Browser2
+
 # Include the device specific makefile
 $(call inherit-product, device/fairphone/FP4/device.mk)
+
+$(call enforce-product-packages-exist,$(allowed_list))
