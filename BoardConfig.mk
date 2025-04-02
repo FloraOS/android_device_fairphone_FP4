021f13bbc77aec808c21be4332639650e02e612b
Bharath <bharath@teamb58.org>
2025-04-02 18:57:22 +0530

    bringup: Initial base files
    Add initial files with basic definitions. The following are currently
    set as part of device configuration:
    
    * Add device architecture
    * Inherit basic product definitions
    * Set up initial product flags and definitions.
    
    Change-Id: I7fdd1627de79505e222b6feb82a40c194286c308

---
+++ BoardConfig.mk
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2024 FairPhone B.V.
+
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
---
+++ device.mk
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2024 FairPhone B.V.
+
+FP_PATH := device/fairphone/FP4
+
+
+# Inherit Virtual AB configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit GSI keys to first stage ramdisk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit generic AOSP content for telephony based 64-bit devices
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+
+# API level the device was shipped
+PRODUCT_SHIPPING_API_LEVEL := 30
+SHIPPING_API_LEVEL := 30
+
+
+PRODUCT_BRAND := Fairphone
+PRODUCT_DEVICE := FP4
+PRODUCT_MANUFACTURER := Fairphone
+
+TARGET_BOARD_PLATFORM := lito
+
+
+# AB configurations
+ENABLE_AB := true # Enable AB partitions by default
+ENABLE_VIRTUAL_AB := true # Enable virtual AB configs by default
+
+
+# Board platforms lists to be used for
+# TARGET_BOARD_PLATFORM specific featurization
+QCOM_BOARD_PLATFORMS += lito
+
+
+# Display Properties
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
