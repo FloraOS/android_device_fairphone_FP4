b7f689bed637845cd9d24d7152cf43483ba1b01d
Karsten Tausche <karsten@fairphone.com>
2026-01-22 15:54:36 +0100

    audio: Increase deferred probe timeout to 300s
    Previous commit device/fairphone/fp4@f914527 "Increase deferred probe
    timeout to 60s" didn't increase the timeout enough for low power
    scenarios. When the phone is booting up on very low battery, CPUs get
    downclocked a lot and booting takes significantly longer. In some tests
    drivers like aw882xx would only probe at around 180s.
    
    Issue: FP4-A15#96
    Change-Id: I27ecb053d02b3f8bcee8522ca3e81ebbf38f3094

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -209,7 +209,7 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
-BOARD_KERNEL_CMDLINE += deferred_probe_timeout=90
+BOARD_KERNEL_CMDLINE += deferred_probe_timeout=300
 ifneq (,$(filter eng,$(TARGET_BUILD_VARIANT)))
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
